fe138cb7294df46c0c1572c26082d050b34dea7b
Lila Amelia <lilaamelia77@gmail.com>
2024-07-01 17:29:09 +0700

    added intro

---
+++ portfolio-website/src/components/Intro.jsx
@@ -0,0 +1,10 @@
+const Intro = () => {
+  return (
+    <div className="container-sm d-flex flex-column flex-wrap justify-content-center my-3">
+      <h3 className="fw-bold">Hello! I&apos;m Lila, and I have a huge passion for designing, particularly in UI/UX. </h3>
+      <p className="lead">With years of experience in frontend development, I&apos;ve honed my skills to create visually appealing, user-friendly, and responsive websites. I&apos;ve developed several projects on my own, showcasing my expertise and creativity. Explore my portfolio to see the projects I&apos;ve brought to life and get a glimpse of my design and development journey.</p>
+    </div>
+  )
+}
+
+export default Intro
